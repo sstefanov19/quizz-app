3b1a9d1e277d445622495bd571d29b2db761d16a
sstefanov <84671344+sstefanov19@users.noreply.github.com>
2024-07-23 11:49:45 +0300

    creating the app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./component/Header";
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Header.jsx
@@ -0,0 +1,7 @@
+import logo from '../assets/quiz-logo.png'
+export default  function Header() {
+    return <header>
+        <img src={logo} alt='logo'/>
+        <h1>ReactQuiz</h1>
+    </header>
+}
